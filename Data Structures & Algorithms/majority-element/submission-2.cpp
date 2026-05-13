6c0d91b3e5b5c9ff3d68ba9d91f135fd456d68fd
Nidhi <nidhidevi425@gmail.com>
2026-05-13 21:16:01 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        // int count=n/2;
+        unordered_map<int,int>mpp;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+        }
+        for(int i=0;i<n;i++){
+            if(mpp[nums[i]]>n/2)
+            return nums[i];
+        }
+        return 0;
+    }
+};
