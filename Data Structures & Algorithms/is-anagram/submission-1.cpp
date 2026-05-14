afdbd8e20226413f9aa0b951469ecb635ab49c2c
Nidhi <nidhidevi425@gmail.com>
2026-05-14 23:29:15 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,28 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//       if(s.size()!=t.size()) return false;
+//         int count[256]={0};
+//         for(int i=0;i<=s.size();i++){
+//             count[s[i]]++;
+//             count[t[i]]--;
+//         }
+//         for(int i=0;i<256;i++){
+//        if(count[i]!=0) return false;
+//         }
+//         return true;
+//     }
+// };
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+      sort(s.begin(),s.end());
+      sort(t.begin(),t.end());
+      if(s==t) return true;
+      else
+      return false;
+    }
+};
+
