2c9a66247d4a2cdd5e0a0374335c1b241409e1ae
Nidhi <nidhidevi425@gmail.com>
2026-06-12 21:43:08 +0530

    Add: rotate-matrix - submission-0

---
+++ Data Structures & Algorithms/rotate-matrix/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<m;j++){
+               swap( matrix[i][j],matrix[j][i]);
+            }
+        }
+for(int i=0;i<n;i++){
+    int s=0,e=n-1;
+    while(s<e){
+        swap(matrix[i][s],matrix[i][e]);
+            s++;
+            e--;
+        }
+    }
+
+
+    }
+};
