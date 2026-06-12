4b2a04b5c63790b7d6203e3f75b31501cd5ae5a1
Nidhi <nidhidevi425@gmail.com>
2026-06-12 20:37:14 +0530

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+long helper(vector<int>& coins,int ind,int t,vector<vector<long>>&dp){
+  if(ind==0){
+    return (t%coins[0]==0);
+  }  
+  if(dp[ind][t]!=-1)
+  return dp[ind][t];
+
+long ntake=helper(coins,ind-1,t,dp);
+long take=0;
+if(coins[ind]<=t){
+ take=helper(coins,ind,t-coins[ind],dp);
+}
+return dp[ind][t]=take+ntake;
+
+}
+    int change(int amount, vector<int>& coins) {
+        int ind=coins.size();
+        vector<vector<long>>dp(ind,vector<long>(amount+1,-1));
+        return helper(coins,ind-1,amount,dp);
+    }
+};
