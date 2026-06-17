7418bf0ecf93e4ec69fd6abded443fa016cab5be
Nidhi <nidhidevi425@gmail.com>
2026-06-17 19:53:30 +0530

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    // FIX 1: Return type ko 'long long' kiya
+    long long digittonumber(vector<int>& digits){
+        int n = digits.size();
+        string s = "";
+        for(int i = 0; i < n; i++){
+            s += to_string(digits[i]);
+        }
+        // FIX 2: stoi ki jagah stoll() kiya taaki string bada number ban sake
+        long long number = stoll(s); 
+        return number;
+    }
+
+    // FIX 3: Input parameter ko 'long long n' kiya
+    vector<int> numberdigit(long long n){
+        vector<int> num;
+        // Edge case: Agar number 0 hai toh seedhe 0 push karein
+        if (n == 0) {
+            num.push_back(0);
+            return num;
+        }
+        while(n){
+            num.push_back(n % 10);
+            n = n / 10;   
+        }
+        reverse(num.begin(), num.end()); 
+        return num;
+    }
+
+    vector<int> plusOne(vector<int>& digits) {
+        long long original = digittonumber(digits);
+        long long incrnumber = original + 1;
+        return numberdigit(incrnumber);
+    }
+};
